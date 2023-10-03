490d7db0e722425212e95effa9cc228ac6cf2022
Chase Balmer <chase.balmer@outlook.com>
2023-10-03 00:38:18 -0400

    File path changes to get application running

--- ufund-api/src/main/java/com/ufund/api/ufundapi/model/Cupboard.java
+++ ufund-api/src/main/java/com/ufund/api/ufundapi/model/Cupboard.java
@@ -1,113 +1,116 @@
-// package com.ufund.api.ufundapi.model;
+package com.ufund.api.ufundapi.model;
 
 
-// import com.fasterxml.jackson.annotation.JsonProperty;
+import com.fasterxml.jackson.annotation.JsonProperty;
 
 
-// import java.util.ArrayList;
-// import java.util.HashMap;
-// import java.util.List;
-// import java.util.Map;
-// import java.util.logging.Logger;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+import java.util.logging.Logger;
 
-// /**
-//  * Need.java
-//  * Description: Class that holds collection of needs for the organization.
-//  * 
-//  * @author - Chase Balmer
-//  */
-// public class Cupboard {
-//     // Private State
-//     private static final Logger LOG = Logger.getLogger(Cupboard.class.getName());
-//     @JsonProperty("currentNeeds") private Map<String,Need> currentNeeds;    /*Needs that are not yet funded */
+import org.springframework.stereotype.Component;
+
+/**
+ * Need.java
+ * Description: Class that holds collection of needs for the organization.
+ * 
+ * @author - Chase Balmer
+ */
+@Component
+public class Cupboard {
+    // Private State
+    private static final Logger LOG = Logger.getLogger(Cupboard.class.getName());
+    @JsonProperty("currentNeeds") private Map<String,Need> currentNeeds;    /*Needs that are not yet funded */
    
-//     private final int INITIAL_SIZE = 45;
+    private final int INITIAL_SIZE = 45;
 
-//     // Constructor 
-//     public Cupboard(){
-//         currentNeeds = new HashMap<>(INITIAL_SIZE);
-//     }
+    // Constructor 
+    public Cupboard(){
+        currentNeeds = new HashMap<>(INITIAL_SIZE);
+    }
 
-//     // Methods
-//    /**
-//     * getNeed
-//     * Description: The function "getNeed" returns a specific need object from the cupboard
-//     * 
-//     * @param wantedNeed requested need
-//     * @return returns wanted need from the cupboard
-//     */
-//     public Need getNeed(String name){
-//         if(name != null){
-//             return currentNeeds.get(name);
-//         }
-//         return null;
-//     }
+    // Methods
+   /**
+    * getNeed
+    * Description: The function "getNeed" returns a specific need object from the cupboard
+    * 
+    * @param wantedNeed requested need
+    * @return returns wanted need from the cupboard
+    */
+    public Need getNeed(String name){
+        if(name != null){
+            return currentNeeds.get(name);
+        }
+        return null;
+    }
 
-//     public boolean isEmpty(){
-//         return currentNeeds.values().size() == 0;
-//     }
+    public boolean isEmpty(){
+        return currentNeeds.values().size() == 0;
+    }
 
-//     /**
-//      * getNeedsOnName
-//      * Description: The function returns a list of needs whose names contain the given text
-//      * 
-//      * @param goalText Text to check for containment
-//      * @return The method is returning a List of Need objects that contain goalText
-//      */
-//     public List<Need> getNeedsOnName(String goalText){
-//         List<Need> matchingNeeds = new ArrayList<>();
-//         if(goalText != null){
-//             for(Need need : currentNeeds.values()){
-//                 if(need.getName().contains(goalText)){
-//                     matchingNeeds.add(need);
-//                 }              
-//             }
-//         }
-//         return matchingNeeds;
-//     }
+    /**
+     * getNeedsOnName
+     * Description: The function returns a list of needs whose names contain the given text
+     * 
+     * @param goalText Text to check for containment
+     * @return The method is returning a List of Need objects that contain goalText
+     */
+    public List<Need> getNeedsOnName(String goalText){
+        List<Need> matchingNeeds = new ArrayList<>();
+        if(goalText != null){
+            for(Need need : currentNeeds.values()){
+                if(need.getName().contains(goalText)){
+                    matchingNeeds.add(need);
+                }              
+            }
+        }
+        return matchingNeeds;
+    }
 
-//     /**
-//      * getEntireCupboard
-//      * Description: The function returns the entire contents of the current cupboard as a map list of need objects.
-//      * 
-//      * @return A list of objects of type Need is being returned.
-//      */
-//     public List<Need> getEntireCupboard(){
-//         return new ArrayList<Need>(currentNeeds.values());
-//     }
+    /**
+     * getEntireCupboard
+     * Description: The function returns the entire contents of the current cupboard as a map list of need objects.
+     * 
+     * @return A list of objects of type Need is being returned.
+     */
+    public List<Need> getEntireCupboard(){
+        return new ArrayList<Need>(currentNeeds.values());
+    }
 
-//     /**
-//      * addNeed
-//      * Description: The function adds a new need to a map of current needs. Will not add if new needs name is taken
-//      * 
-//      * @param newNeed Need to be added
-//      * @return boolean based on succes of adding
-//      */
-//     public boolean addNeed(Need newNeed){
-//         if(newNeed != null && !(this.currentNeeds.containsKey(newNeed.getName()))){
-//             this.currentNeeds.put(newNeed.getName(), newNeed);
-//             return true;
-//         }
-//         return false;
-//     }
+    /**
+     * addNeed
+     * Description: The function adds a new need to a map of current needs. Will not add if new needs name is taken
+     * 
+     * @param newNeed Need to be added
+     * @return boolean based on succes of adding
+     */
+    public boolean addNeed(Need newNeed){
+        if(newNeed != null && !(this.currentNeeds.containsKey(newNeed.getName()))){
+            this.currentNeeds.put(newNeed.getName(), newNeed);
+            return true;
+        }
+        return false;
+    }
 
-//     /**
-//      * This function removes a need from a collection of needs and returns true if the need was
-//      * successfully removed.
-//      * 
-//      * @param needName string representing the name of the need to be removed.
-//      * @return boolean based on success of removal
-//      */
-//     public boolean removeNeed(String needName){
-//         if(needName != null){
-//             Need removedNeed = currentNeeds.remove(needName);
-//             return removedNeed != null;
-//         }
-//         return false;
-//     }
+    /**
+     * This function removes a need from a collection of needs and returns true if the need was
+     * successfully removed.
+     * 
+     * @param needName string representing the name of the need to be removed.
+     * @return boolean based on success of removal
+     */
+    public boolean removeNeed(String needName){
+        if(needName != null){
+            Need removedNeed = currentNeeds.remove(needName);
+            return removedNeed != null;
+        }
+        return false;
+    }
 
-//     @Override
-//     public String toString() {
-//         return String.format(getEntireCupboard().toString());
-//     }
-// }
+    @Override
+    public String toString() {
+        return String.format(getEntireCupboard().toString());
+    }
+}
