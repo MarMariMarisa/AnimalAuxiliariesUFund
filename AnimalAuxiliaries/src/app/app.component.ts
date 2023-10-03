87fd3d9e836b6e25aae9bb2bd76807d1bc6fde10
tc6386 <tc6386@rit.edu>
2023-10-03 16:39:06 -0400

    Need class testing

--- AnimalAuxiliaries/src/app/app.component.ts
+++ AnimalAuxiliaries/src/app/app.component.ts
@@ -8,23 +8,3 @@ import { Component } from '@angular/core';
 export class AppComponent {
   title = 'AnimalAuxiliaries';
 }
-const fundButton = document.getElementById('fundButton');
-if (fundButton) {
-  fundButton.addEventListener('onclick', (e) => {
-    console.log('hello');
-    let funding = <HTMLInputElement>document.getElementById('fundingRecipient');
-    if (funding) {
-      let target = `isFunded${funding.value}`;
-      const isNeedFunded = document.getElementById(target);
-      if (isNeedFunded) {
-        isNeedFunded.classList.toggle('successful');
-        isNeedFunded.classList.toggle('unsuccessful');
-        if (isNeedFunded.classList.contains('successful')) {
-          isNeedFunded.textContent = 'Funded!';
-        } else {
-          isNeedFunded.textContent = 'Not Funded!';
-        }
-      }
-    }
-  });
-}
