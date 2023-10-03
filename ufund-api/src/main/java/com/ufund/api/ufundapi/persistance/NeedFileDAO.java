f02824fa232c4fddcd854bc905e6b68e98d0e385
tec8354 <tec8354@rit.edu>
2023-10-02 21:41:01 -0400

    added tiers and layers diagram and domain model

--- ufund-api/src/main/java/com/ufund/api/ufundapi/persistance/NeedFileDAO.java
+++ ufund-api/src/main/java/com/ufund/api/ufundapi/persistance/NeedFileDAO.java
@@ -2,6 +2,45 @@ package com.ufund.api.ufundapi.persistance;
 
 import java.io.IOException;
 
+import com.ufund.api.ufundapi.model.Cupboard;
+import com.ufund.api.ufundapi.model.Need;
+
 public class NeedFileDAO implements NeedDAO{
+
+    @Override
+    public Need[] getNeeds(Cupboard cupboard) throws IOException {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'getNeeds'");
+    }
+
+    @Override
+    public Need[] searchNeedsByName(String text) throws IOException {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'searchNeedsByName'");
+    }
+
+    @Override
+    public Need getNeed(String id) throws IOException {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'getNeed'");
+    }
+
+    @Override
+    public Need createNeed() throws IOException {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'createNeed'");
+    }
+
+    @Override
+    public Need updateNeed(Need need) throws IOException {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'updateNeed'");
+    }
+
+    @Override
+    public Need deleteNeed(String id) throws IOException {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'deleteNeed'");
+    }
     
 }
