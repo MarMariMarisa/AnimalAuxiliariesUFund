c4b97e60dbef7cbe64dc70d3c8daed2e114e5e69
Chase Balmer <chase.balmer@outlook.com>
2023-10-22 15:47:24 -0400

    FundingBasket

---
+++ AnimalAuxiliaries/src/main/java/com/ufund/api/ufundapi/model/FundingBasket.java
@@ -0,0 +1,33 @@
+package main.java.com.ufund.api.ufundapi.model;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import com.ufund.api.ufundapi.model.Need;
+
+public class FundingBasket {
+    // Private State
+    private List<Need> needs;
+
+    // Constructor
+    public FundingBasket(){
+        needs = new ArrayList<>();
+    }
+
+    // Methods
+    public List<Need> getBasket(){
+        return needs;
+    }
+
+    public boolean addToBasket(Need need){
+        // Check is real need that needs funding
+        if(need != null & (need.getAllFunded() == false)){
+            return needs.add(need);
+        }
+        return false;
+    }
+
+    public boolean removeFromBasket(Need need){
+        return needs.remove(need);     
+    }
+}
--- AnimalAuxiliaries/src/main/java/com/ufund/api/ufundapi/model/Need.java
+++ AnimalAuxiliaries/src/main/java/com/ufund/api/ufundapi/model/Need.java
@@ -91,7 +91,7 @@ public class Need {
             return false;
         }
         Need other = (Need) obj;
-        return id.equals(other.id);
+        return id.equals(other.getId());
 }
 
     /**
