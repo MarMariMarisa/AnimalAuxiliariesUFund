05f5392f3a8722dbf6cf567262686e7897cde4fc
Sarah Payne <sap4735@rit.edu>
2023-10-23 17:29:54 -0400

    Created and stubbed out UserDAO files

---
+++ AnimalAuxiliaries/src/main/java/com/ufund/api/ufundapi/persistence/UserDAO.java
@@ -0,0 +1,14 @@
+package com.ufund.api.ufundapi.persistence;
+
+import java.io.IOException; 
+
+import com.ufund.api.ufundapi.model.User;
+
+public interface UserDAO {
+
+    //Should be pretty minimal, really just exists so that the managers and the helpers can be stored in the same place, I think? 
+    
+    //TO DO getUsername 
+
+    //TO DO getID ? 
+}
