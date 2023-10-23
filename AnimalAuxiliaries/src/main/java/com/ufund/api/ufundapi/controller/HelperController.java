a20302a8e92422a4ca0ea710d6c766c3cdcf1578
Sarah Payne <sap4735@rit.edu>
2023-10-23 17:29:39 -0400

    Stubbed out HelperController.java

--- AnimalAuxiliaries/src/main/java/com/ufund/api/ufundapi/controller/HelperController.java
+++ AnimalAuxiliaries/src/main/java/com/ufund/api/ufundapi/controller/HelperController.java
@@ -1,7 +1,12 @@
 package com.ufund.api.ufundapi.controller;
 
+import java.util.logging.Logger;
+
+import com.ufund.api.ufundapi.model.Helper; 
+
 public class HelperController {
+    private static final Logger LOG = Logger.getLogger(HelperController.class.getName()); 
+    private Helper helper; 
 
-
-
+    
 }
