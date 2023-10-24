83bba6e0112e4dd1c556b0ba2aa3c7fa8a769152
Chase Balmer <chase.balmer@outlook.com>
2023-10-24 12:55:46 -0400

    Start of helper persistence

--- AnimalAuxiliaries/src/main/java/com/ufund/api/ufundapi/controller/HelperController.java
+++ AnimalAuxiliaries/src/main/java/com/ufund/api/ufundapi/controller/HelperController.java
@@ -1,12 +1,27 @@
 package com.ufund.api.ufundapi.controller;
 
+import java.io.IOException;
 import java.util.logging.Logger;
 
-import com.ufund.api.ufundapi.model.Helper; 
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
+import com.ufund.api.ufundapi.persistence.HelperFileDAO; 
+
+/*
+ * Cupboard controller is still being used to add/remove stuff from the cupboard 
+ * This class is going to be used for adding/removing/viewing from funding baskets 
+ * 
+ */
+@RestController
+@RequestMapping("fundingbasket")
 public class HelperController {
     private static final Logger LOG = Logger.getLogger(HelperController.class.getName()); 
-    private Helper helper; 
+    private HelperFileDAO helperDAO;
 
-    
+    public HelperController(HelperFileDAO helperDAO) throws IOException {
+        this.helperDAO = helperDAO;
+    }
+
+    // Mapping can mirror other stuff
 }
