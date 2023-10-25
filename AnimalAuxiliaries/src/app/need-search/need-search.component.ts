871d9771fc6ec675294a1f8e4a3ff15d8ef7e83c
Sarah Payne <sap4735@rit.edu>
2023-10-25 16:12:06 -0400

    Made Search more pretty

--- AnimalAuxiliaries/src/app/need-search/need-search.component.ts
+++ AnimalAuxiliaries/src/app/need-search/need-search.component.ts
@@ -4,6 +4,10 @@ import { Need } from '../need';
 import { Observable, Subject } from 'rxjs';
 import { CupboardService } from '../cupboard.service';
 import { debounceTime, distinctUntilChanged, switchMap } from 'rxjs/operators';
+
+import { FundingBasketService } from '../funding-basket.service';
+import { AuthService } from '../auth.service';
+
 @Component({
   selector: 'app-need-search',
   templateUrl: './need-search.component.html',
@@ -13,11 +17,22 @@ export class NeedSearchComponent implements OnInit {
   needs$!: Observable<Need[]>;
   private searchTerms = new Subject<string>();
 
-  constructor(private cupboardService: CupboardService) {}
+  constructor(
+    private cupboardService: CupboardService,
+    private fundingbasketService: FundingBasketService,
+    private auth: AuthService
+    ) {
+  }
 
   // Push a search term into the observable stream.
   search(term: string): void {
     this.searchTerms.next(term);
+  }
+
+  addToBasket(need: Need): void {
+    this.fundingbasketService
+      .addToBasket(this.auth.getUsername(),need)
+      .subscribe((newNeed) => newNeed);
   }
   ngOnInit(): void {
     this.needs$ = this.searchTerms.pipe(
