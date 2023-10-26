a06a45586a3c642c416b0632efd5aec793707655
Chase Balmer <66214702+cjb7988@users.noreply.github.com>
2023-10-25 22:28:48 -0400

    Update Manager.java

--- AnimalAuxiliaries/src/main/java/com/ufund/api/ufundapi/model/Manager.java
+++ AnimalAuxiliaries/src/main/java/com/ufund/api/ufundapi/model/Manager.java
@@ -1,119 +1,119 @@
-package com.ufund.api.ufundapi.model;
+// package com.ufund.api.ufundapi.model;
 
-import java.util.List;
-import java.util.UUID;
+// import java.util.List;
+// import java.util.UUID;
 
-import com.fasterxml.jackson.annotation.JsonProperty;
+// import com.fasterxml.jackson.annotation.JsonProperty;
 
-/**
- * Manager class is not used in the MVP and this class will remain in development until 
- * if/when we choose to use it in Sprint 3
- */
-public class Manager implements User {
+// /**
+//  * Manager class is not used in the MVP and this class will remain in development until 
+//  * if/when we choose to use it in Sprint 3
+//  */
+// public class Manager implements User {
 
-    @JsonProperty("id")
-    private final String id;
+//     @JsonProperty("id")
+//     private final String id;
 
-    @JsonProperty("username")
-    private final String username;
+//     @JsonProperty("username")
+//     private final String username;
 
-    private Cupboard cupboard;
+//     private Cupboard cupboard;
 
-    private static final String MANAGER_ID_MODIFIER = "M";
+//     private static final String MANAGER_ID_MODIFIER = "M";
 
-    public Manager(String username, Cupboard cupboard) {
-        this.username = username;
-        this.cupboard = cupboard;
-        this.id = MANAGER_ID_MODIFIER + UUID.randomUUID().toString();
-    }
+//     public Manager(String username, Cupboard cupboard) {
+//         this.username = username;
+//         this.cupboard = cupboard;
+//         this.id = MANAGER_ID_MODIFIER + UUID.randomUUID().toString();
+//     }
 
-    //view cupboard
-    /**
-     * 
-     * @return a list of all needs in the cupboard
-     */
-    public List<Need> viewCupboard(){
-        return cupboard.getEntireCupboard();
-    }
+//     //view cupboard
+//     /**
+//      * 
+//      * @return a list of all needs in the cupboard
+//      */
+//     public List<Need> viewCupboard(){
+//         return cupboard.getEntireCupboard();
+//     }
 
-    //search
-    /**
-     * 
-     * @param substring string to search through
-     * @return list of needs containing the string
-     */
-    public List<Need> searchNeeds(String substring) {
-        return cupboard.getNeedsOnName(substring);
-    }
+//     //search
+//     /**
+//      * 
+//      * @param substring string to search through
+//      * @return list of needs containing the string
+//      */
+//     public List<Need> searchNeeds(String substring) {
+//         return cupboard.getNeedsOnName(substring);
+//     }
 
-    //select
-    /**
-     * 
-     * @param id string representing the need id
-     * @return the need in cupboard matching the id
-     */
-    public Need getNeed(String id){
-        return cupboard.getNeedOnID(id);
-    }
+//     //select
+//     /**
+//      * 
+//      * @param id string representing the need id
+//      * @return the need in cupboard matching the id
+//      */
+//     public Need getNeed(String id){
+//         return cupboard.getNeedOnID(id);
+//     }
 
-    //edit -> should also remove from any helper's funding basket
-    /**
-     * 
-     * @param need the need being updated
-     * @return boolean value if need is succesfully updated
-     */
-    public boolean updateNeed(Need need){
-        return cupboard.updateNeed(need);
-    }
+//     //edit -> should also remove from any helper's funding basket
+//     /**
+//      * 
+//      * @param need the need being updated
+//      * @return boolean value if need is succesfully updated
+//      */
+//     public boolean updateNeed(Need need){
+//         return cupboard.updateNeed(need);
+//     }
 
-    //retire -> should also remove from any helper's funding basket
-    /**
-     * 
-     * @param needId id of the need to be retired
-     * @return boolean value if the need is successfully retired
-     */
-    public boolean retireNeed(String needId){
-        return cupboard.retireNeed(needId);
-    }
+//     //retire -> should also remove from any helper's funding basket
+//     /**
+//      * 
+//      * @param needId id of the need to be retired
+//      * @return boolean value if the need is successfully retired
+//      */
+//     public boolean retireNeed(String needId){
+//         return cupboard.retireNeed(needId);
+//     }
 
-    //unretire
-    /**
-     * 
-     * @param needId id of the need to be unretired
-     * @return boolean value if the need is successfully unretired
-     */
-    public boolean unretireNeed(String needId){
-        return cupboard.unretireNeed(needId);
-    }
+//     //unretire
+//     /**
+//      * 
+//      * @param needId id of the need to be unretired
+//      * @return boolean value if the need is successfully unretired
+//      */
+//     public boolean unretireNeed(String needId){
+//         return cupboard.unretireNeed(needId);
+//     }
 
-    //create
-    /**
-     * 
-     * @param name 
-     * @param description
-     * @param type
-     * @param price
-     * @param quantity
-     * @return boolean value if the need is succesfully created and added to the cupboard
-     */
-    public boolean createNewNeed(String name, String description, String type, float price, int quantity){
-        Need newneed = new Need(name, description, type, price, quantity);
-        return cupboard.addNeed(newneed);
-    }
+//     //create
+//     /**
+//      * 
+//      * @param name 
+//      * @param description
+//      * @param type
+//      * @param price
+//      * @param quantity
+//      * @return boolean value if the need is succesfully created and added to the cupboard
+//      */
+//     public boolean createNewNeed(String name, String description, String type, float price, int quantity){
+//         Need newneed = new Need(name, description, type, price, quantity);
+//         return cupboard.addNeed(newneed);
+//     }
 
-    /**
-     * @return returns Manager id
-     */
-    @Override
-    public String getId() {
-        return this.id;
-    }
+//     /**
+//      * @return returns Manager id
+//      */
+//     @Override
+//     public String getId() {
+//         return this.id;
+//     }
 
-    /**
-     * @return returns Manager username
-     */
-    @Override
-    public String getUsername() {
-        return this.username;
-    }
-}
+//     /**
+//      * @return returns Manager username
+//      */
+//     @Override
+//     public String getUsername() {
+//         return this.username;
+//     }
+// }
