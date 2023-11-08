f6ee5e80f21350901fc0651996050cf705fbc4d6
Chase Balmer <chase.balmer@outlook.com>
2023-11-08 16:46:08 -0500

    Checkout Tests

--- AnimalAuxiliaries/src/main/java/com/ufund/api/ufundapi/model/FundingBasket.java
+++ AnimalAuxiliaries/src/main/java/com/ufund/api/ufundapi/model/FundingBasket.java
@@ -4,42 +4,43 @@ import java.util.ArrayList;
 import java.util.List;
 
 import com.fasterxml.jackson.annotation.JsonFormat;
-import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 import com.fasterxml.jackson.annotation.JsonProperty;
 
 @JsonFormat(shape = JsonFormat.Shape.OBJECT)
 public class FundingBasket {
-   // Private State
+    // Private State
     @JsonProperty("needs")
     private List<Need> needs;
 
     // Constructor
-    public FundingBasket(){
+    public FundingBasket() {
         needs = new ArrayList<>();
     }
-
-    // Methods
-    // public List<Need> getBasket(){
-    //     return needs;
-    // }
 
     public Need[] getNeeds() {
         return needs.toArray(new Need[0]);
     }
 
-    public boolean addToBasket(Need need){
+    public boolean addToBasket(Need need) {
         // Check is real need that needs funding
-        if(need != null && need.getAllFunded() == false){
+        if (need != null && need.getAllFunded() == false) {
             return needs.add(need);
         }
         return false;
     }
 
-    // public List<Need> getFundingBasket() {
-    //     return needs;
-    // }
+    public boolean removeFromBasket(Need need) {
+        return needs.remove(need);
+    }
 
-    public boolean removeFromBasket(Need need){
-        return needs.remove(need);     
-    } 
+    // Purely empties the shopping cart
+    public boolean checkout() {
+        needs.clear();
+        return true;
+    }
+
+    public List<Need> getBasket(){
+        return needs;
+    }    
+
 }
--- AnimalAuxiliaries/src/main/java/com/ufund/api/ufundapi/model/Helper.java
+++ AnimalAuxiliaries/src/main/java/com/ufund/api/ufundapi/model/Helper.java
@@ -1,16 +1,12 @@
 package com.ufund.api.ufundapi.model;
 
-import java.util.List;
 import java.util.UUID;
 
 import com.fasterxml.jackson.annotation.JsonIgnore;
-import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 import com.fasterxml.jackson.annotation.JsonProperty;
-import com.fasterxml.jackson.annotation.JsonUnwrapped;
 
-// @JsonIgnoreProperties(ignoreUnknown = true)
 public class Helper implements User {
-    
+
     @JsonProperty("id")
     private final String id;
     @JsonProperty("username")
@@ -20,54 +16,56 @@ public class Helper implements User {
     @JsonProperty("basket")
     private FundingBasket fundingBasket;
 
-    private static final String HELPER_ID_MODIFIER = "H"; 
+    private static final String HELPER_ID_MODIFIER = "H";
+
     public Helper(String username, String password) {
         this.username = username;
         this.password = password;
-        this.id = HELPER_ID_MODIFIER + UUID.randomUUID().toString(); 
+        this.id = HELPER_ID_MODIFIER + UUID.randomUUID().toString();
         this.fundingBasket = new FundingBasket();
     }
+
     public Helper(String username) {
         this.username = username;
         this.password = "";
-        this.id = HELPER_ID_MODIFIER + UUID.randomUUID().toString(); 
+        this.id = HELPER_ID_MODIFIER + UUID.randomUUID().toString();
         this.fundingBasket = new FundingBasket();
     }
-    
-    public Helper(){
+
+    public Helper() {
         this.username = "";
         this.password = "";
         this.id = HELPER_ID_MODIFIER + UUID.randomUUID().toString();
         this.fundingBasket = new FundingBasket();
     }
 
-    public boolean addToFundingBasket(Need need){
+    public boolean addToFundingBasket(Need need) {
         return fundingBasket.addToBasket(need);
     }
 
-    public boolean removeFromFundingBasket(Need need){
-        return fundingBasket.removeFromBasket(need);   
+    public boolean removeFromFundingBasket(Need need) {
+        return fundingBasket.removeFromBasket(need);
     }
-    
-    @JsonIgnore 
+
+    @JsonIgnore
     public Need[] getBasketNeeds() {
         return fundingBasket.getNeeds();
     }
 
-    // @JsonUnwrapped
-    // public FundingBasket getFundingBasket() {
-    //     return fundingBasket;
-    // }
     public String getPassword() {
         return this.password;
     }
+
     public String getId() {
-        return this.id; 
+        return this.id;
     }
 
     public String getUsername() {
-        return this.username; 
+        return this.username;
+    }
+
+    // Clears funding basket 
+    public boolean checkout() {
+        return fundingBasket.checkout();
     }
 }
-
-
