44deb01621eee2e2019783dd335a0e86ee586ebb
tc6386 <tc6386@rit.edu>
2023-11-16 15:49:14 -0500

    bugfixes

--- AnimalAuxiliaries/src/app/community-board/community-board.component.ts
+++ AnimalAuxiliaries/src/app/community-board/community-board.component.ts
@@ -1,4 +1,3 @@
-
 import { ChangeDetectorRef, Component } from '@angular/core';
 import { CommunityBoardService } from '../community-board.service';
 import { Post } from '../post';
@@ -7,24 +6,21 @@ import { Observable } from 'rxjs';
 @Component({
   selector: 'app-community-board',
   templateUrl: './community-board.component.html',
-  styleUrls: ['./community-board.component.css']
+  styleUrls: ['./community-board.component.css'],
 })
 export class CommunityBoardComponent {
   constructor(
     private communityBoardService: CommunityBoardService,
-    private changeDetection: ChangeDetectorRef,
+    private changeDetection: ChangeDetectorRef
   ) {}
-  posts$!: Observable<Post[]>;
   currentPosts: Post[] = [];
 
   ngOnInit(): void {
     this.getCommunityBoard();
-   
   }
   getCommunityBoard(): void {
     this.communityBoardService
       .getCommunityBoard()
-      .subscribe((post) => (this.currentPosts = [...post]));
+      .subscribe((post) => (this.currentPosts = [...post].reverse()));
   }
-
 }
